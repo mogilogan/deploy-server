e1ea7450d62ad1a4b0df3fca3fda862f360b9e20
Moginder <mogiegan@gmail.com>
2023-01-07 12:12:43 +0530

    server tst

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ const PORT = process.env.PORT || 8000;
 app = express();
 
 app.use(cors({
-  origin:'http://localhost:3000/'
+  origin:'*'
 }));
 app.use(express.json());
 
 
 app.get('/', async (req, res)=>{
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.send(data);
 })
 
